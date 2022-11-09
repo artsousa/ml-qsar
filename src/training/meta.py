91a7abebc93cbbdaadb2d2f9ea8d4e67a9c66f11
arthur <arthursvrr@gmail.com>
2022-11-09 22:08:59 +0000

    meta-training structure

--- src/training/meta.py
+++ src/training/meta.py
@@ -11,8 +11,20 @@ import torch.nn as nn
 from src import utils
 
 from fs_mol.data import (
-    FSMolDataset
+    DataFold,
+    FSMolDataset,
 )
+
+logger = logging.getLogger()
+
+
+def save_checkpoint(model: nn.Module, save_path: str, step: Union[str, int]):
+    ckpt_name = f"step_{step}"
+    torch.save(model, os.path.join(save_path, f"{ckpt_name}.pth"))
+    torch.save(
+        model.state_dict(),
+        os.path.join(save_path, f"{ckpt_name}_state_dict.pth"),
+    )
 
 def meta_training(
     meta_learner,
@@ -28,8 +40,26 @@ def meta_training(
     metrics: List[str] = ["accuracy_score"],
 ) -> None:
     
-    pass
-    # if meta_batch_size > len(loaders["meta_train"]["train"]):
-    #     raise ValueError(
-    #         f"meta_batch_size ({meta_batch_size}) must <= number of tasks ({len(loaders['meta_train']['train'])})"
-    #     )
+    total_train_tasks = dataset.get_num_fold_tasks(DataFold.TRAIN)
+    if meta_batch_size > total_train_tasks:
+        raise ValueError(
+            f"meta_batch_size ({meta_batch_size}) must <= number of tasks ({total_train_tasks})"
+        )
+
+    summary_dict = {}
+    summary_dict["meta_training_loss"] = []
+    summary_dict["meta_training_metrics"] = {m: [] for m in metrics}
+
+    save_checkpoint(model=meta_learner.module, save_path=save_path, step="init")
+    logger.info("Initial checkpoint saved.")
+
+    for step in range(meta_steps):
+        logger.info(f"Iteration {step}")
+
+        iteration_loss = 0.0
+        meta_train_loss = 0.0
+        meta_training_metrics = {m: 0 for m in metrics}
+
+        # Meta training
+
+        logging.info(f"meta_training_loss: {meta_train_loss}")
